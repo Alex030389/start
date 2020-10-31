e3102b44cbebeca7aa13da17a5b86ff60f1b2529
Alex Ka <x182838@gmail.com>
2020-10-31 10:08:44 +0300

    module stick footer

---
+++ _src/static/script/block/__stick-footer.js
@@ -0,0 +1,22 @@
+// ///////////////////////////////////////////////////// stick footer
+(function () {
+  let isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+
+  let stickFooter = function () {
+    let FOOTER = document.querySelector('footer');
+    let MAIN = document.querySelector('main');
+    let BODY = document.querySelector('body');
+    let footerHeight = FOOTER.offsetHeight;
+    BODY.style.position = 'relative';
+    MAIN.style.marginBottom = footerHeight + 'px';
+    FOOTER.style.position = 'absolute';
+    FOOTER.style.bottom = '0';
+    FOOTER.style.left = '0';
+    FOOTER.style.width = '100%';
+  };
+
+  if (isIE11) {
+    stickFooter();
+    window.addEventListener('resize', stickFooter);
+  };
+})();
